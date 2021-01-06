7d53efa6d69d101cb4c57cab0627b9d6d19a845d
sivanyo <57237255+sivanyo@users.noreply.github.com>
2021-01-06 17:01:54 +0200

    finished ID3 Success rate - 96.428 %

--- ID3.py
+++ ID3.py
@@ -1,81 +1,118 @@
 import numpy as np
 import pandas as pd
 
-# example - e[0]- ex num, e[1]- class, e[2]-e[last] : features results, e[feature_index] -> e[feature_index+2]
 
+# example e[0]- class, e[1]-e[last] : features results, e[feature_index] -> e[feature_index+2]
 
-def is_consistent(examples, class_c):
+def is_consistent(examples):
+
+    if len(examples) == 1:
+        example = examples[0]
+        class_c = example[1]
+        # print("got here")
+        return True, class_c
+    example1 = examples[0]
+    class_c = example1[0]
     for e in examples:
         if e[0] is not class_c:
-            return False
-    return True
+            return False, None
+    return True, class_c
 
 
 def entropy(x, y):
     if x is 0 or y is 0:
         return 0
     sum = x + y
-    entropy = ((-x * np.log2(x / sum) / sum) - (-y * np.log2(y / sum) / sum))
-    #print(entropy)
+    entropy = (((-x/sum) * np.log2(x / sum)) - ((y/sum) * np.log2(y / sum)))
+    # print(entropy)
+    # print(entropy)
     return entropy
 
 
-def information_gain(examples_group, feature_index, class_c):
-    values = []
-    for item in examples_group:
-        values.append(float(item[feature_index+1]))
-    values = np.unique(values)
-    final_values = []
-    for i in range(len(values) - 1):
-        if values[i] is not values[i + 1]:
-            final_values.append((float(values[i]) + float(values[i + 1])) / 2)
-    best_val = -1
-    min_entropy = 0
-    for val in final_values:
-        min_e = float('-inf')
-        min_val = -1
-        lower, bigger = [], []
-        for ex in examples_group:
-            if float(ex[feature_index + 1]) < val:
-                lower.append(ex)
-            else:
-                bigger.append(ex)
-        lower_type_1, lower_type_2 = [], []
-        for ex in lower:
-            if ex[0] == class_c:
-                lower_type_1.append(ex)
-            else:
-                lower_type_2.append(ex)
-        lower_entropy = entropy(len(lower_type_1), len(lower_type_2))
+def information_gain(examples_group, class_c):
+    # maxIG iff min entro
+    size = len(examples_group)
+    min_entro = float('inf')
+    selected_feature = -1
+    higher_final, lower_final = [], []
+    split_val = None
+    for i in range(1, len(examples_group[0])):
+        # print(i)
+        tmp_min_entro = float('inf')
+        tmp_split_val = None
+        values = []
+        for item in examples_group:
+            if float(item[i]) not in values:
+                values.append(float(item[i]))
+        values = np.unique(values)
 
-        # calculate bigger entropy
-        bigger_type_1, bigger_type_2 = [], []
-        for ex in bigger:
-            if ex[0] == class_c:
-                bigger_type_1.append(ex)
-            else:
-                bigger_type_2.append(ex)
-        bigger_entropy = entropy(len(bigger_type_1), len(bigger_type_2))
+        final_values = []
+        for j in range(len(values) - 1):
+            if values[j] is not values[j + 1]:
+                final_values.append((float(values[j]) + float(values[j + 1])) / 2)
+        tmp_h, tmp_l = [], []
+        for j in range(len(final_values)):
+            #print(j)
+            lower, higher = [], []
+            for ex in examples_group:
+                #print(ex[i]< final_values[j])
+                if float(ex[i]) < final_values[j]:
+                    lower.append(ex)
+                else:
+                    higher.append(ex)
+            lower_type_1, lower_type_2 = [], []
+            for ex in lower:
+                if ex[0] == class_c:
+                    lower_type_1.append(ex)
+                else:
+                    lower_type_2.append(ex)
 
-        tmp_entropy = (len(lower) * lower_entropy + len(bigger) * bigger_entropy) / (len(lower) + len(bigger))
-        if tmp_entropy < min_e:
-            min_e = tmp_entropy
-            min_val = val
-            inner_lower = lower
-            inner_bigger = bigger
-    if min_e < min_entropy:
-        best_val = min_val
-        min_entropy = min_e
-    return min_entropy, best_val
+            lower_entropy = entropy(len(lower_type_1), len(lower_type_2))
+
+            higher_type_1, higher_type_2 = [], []
+            for ex in higher:
+                if ex[0] == class_c:
+                    higher_type_1.append(ex)
+                else:
+                    higher_type_2.append(ex)
+            higher_entropy = entropy(len(higher_type_1), len(higher_type_2))
+            tmp_entro = (len(lower) * lower_entropy + len(higher) * higher_entropy) / size
+            # print(tmp_entro)
+            if tmp_entro < tmp_min_entro:
+                tmp_min_entro = tmp_entro
+                tmp_split_val = final_values[j]
+                tmp_h = higher
+                tmp_l = lower
+        # print("tmp min entro")
+        # print(tmp_min_entro)
+        if tmp_min_entro < min_entro:
+            min_entro = tmp_min_entro
+            split_val = tmp_split_val
+            selected_feature = i
+            higher_final = tmp_h
+            lower_final = tmp_l
+    return selected_feature, split_val, lower_final, higher_final
+
+
+def split(feature_index, val, example_group):
+    left, right = [], []
+    # print(feature_index)
+    for ex in example_group:
+        if ex[feature_index] < val:
+            left.append(ex)
+        else:
+            right.append(ex)
+    return left, right
 
 
 class Node:
-    def __init__(self, split_feature, split_val, left, right):
-        self.split_feature = split_feature
+    def __init__(self, one_class_type):
+        self.split_feature = None
         # bigger than val -> right, else -> left
-        self.split_val = split_val
-        self.right = right
-        self.left = left
+        self.split_val = None
+        self.right = None
+        self.left = None
+        self.one_class_type = one_class_type
         self.classification = None
 
     def find_class_by_example(self, example):
@@ -84,9 +121,28 @@ class Node:
             return -1
         if self.classification is not None:
             return self.classification
-        elif example[self.split_feature + 1] <= self.split_val:
+        elif example[self.split_feature] < self.split_val:
             return self.left.find_class_by_example(example)
         return self.right.find_class_by_example(example)
+
+    def build(self, examples):
+        if len(examples) == 0:
+            print("empty node")
+            return
+        res, classification = is_consistent(examples)
+
+        if res:
+            # print("consistent node")
+            self.classification = classification
+            return
+
+        self.split_feature, self.split_val, left, right = information_gain(examples, self.one_class_type)
+        self.left = Node(self.one_class_type)
+        self.right = Node(self.one_class_type)
+        if len(left) != 0:
+            self.left.build(left)
+        if len(right) != 0:
+            self.right.build(right)
 
 
 class ID3:
@@ -95,30 +151,15 @@ class ID3:
         self.data_arr = self.load_data('train.csv')
         # print(self.data_arr)
         self.examples = self.data_arr[1:]
-        self.test = None
+        # self.test = None
         self.classes = self.find_classes()
-        root = Node(None, None, None, None)
+        self.root = Node(self.classes[0])
         self.num_of_features = len(self.examples[0]) - 2
         print("start your training, good luck")
-        self.tree = self.fit(self.data_arr, None, root)
 
     @staticmethod
     def load_data(filename):
-        # data = pd.read_csv(filename)
-        # # print(data)
-        # string_data = pd.DataFrame.to_string(data)
-        # list_results_tmp = string_data.splitlines()
-        # data_arr = []
-        # for line in list_results_tmp:
-        #     tmp = line.split(',')
-        #     list_data = tmp[0].split()
-        #     data_arr.append(list_data)
-        # return data_arr
-        # print(np.loadtxt(filename, delimiter=',', skiprows=1))
-        # data = pd.read_csv(filename, delimiter=',')
-        #csv = np.genfromtxt('test.csv', dtype='unicode', delimiter=",")
         data = pd.read_csv(filename, sep=',')
-
         return data.values.tolist()
 
     def find_classes(self):
@@ -127,6 +168,9 @@ class ID3:
             if item[0] not in classes:
                 classes.append(item[0])
         return classes
+
+    def train(self):
+        self.root.build(self.examples)
 
     def majority_class(self, examples):
         class_dict = {}
@@ -142,83 +186,22 @@ class ID3:
         # print(majority_class_res)
         return majority_class_res
 
-    @staticmethod
-    def select_feature(num_of_features, examples, class_c):
-        feature_index = -1
-        max_IG = 0.0
-        split_val = None
-        for i in range(num_of_features):
-            tmp_IG = information_gain(examples, i, class_c)
-
-            if tmp_IG[0] < max_IG:
-                max_IG = tmp_IG[0]
-                feature_index = i
-                split_val = tmp_IG[1]
-            elif tmp_IG == max_IG:
-                if i > feature_index:
-                    feature_index = i
-                    split_val = tmp_IG[1]
-        return feature_index, split_val
-
-    def fit(self, examples, default, node):
-        if examples is []:
-            # means the leaf is empty
-            node.classification = default
-            print("got to leaf node, no more examples !")
-            return node
-
-        class_c = self.majority_class(examples)
-        # print(class_c)
-
-        if is_consistent(examples, class_c):
-            # means the node is consistent, make it a leaf
-            node.classification = class_c
-            print("you are consistent node, lets make you a leaf")
-            return node
-
-        feature_f = self.select_feature(self.num_of_features, examples, class_c)
-        node.split_val = feature_f[1]
-        node.split_feature = feature_f[0]
-
-        feature_val = feature_f[1]
-        left, right = self.split(feature_f[0], feature_val, examples)
-
-        default_left = self.majority_class(left)
-        left_node = Node(feature_f, feature_val, None, None)
-        node.left = self.fit(left, default_left, left_node)
-
-        default_right = self.majority_class(right)
-        right_node = Node(feature_f, feature_val, None, None)
-        node.right = self.fit(right, default_right, right_node)
-
-        print(node)
-
-        return node
-
-    @staticmethod
-    def split(feature_index, val, example_group):
-        left, right = [], []
-        # print(feature_index)
-        for ex in example_group:
-            if ex[feature_index + 1] < val:
-                left.append(ex)
-            else:
-                right.append(ex)
-        return left, right
-
     def test(self, train_filename):
         print("start your test !!! test score:")
-        self.test = self.load_data(train_filename)
-        examples = self.test[1:]
+        tester = self.load_data(train_filename)
+        examples = tester[1:]
         right, wrong = 0, 0
         for i in range(len(examples)):
-            if self.tree.find_class_by_example(examples[i]) is self.test[i + 1][0]:
+            if self.root.find_class_by_example(examples[i]) is tester[i + 1][0]:
                 right += 1
             else:
                 wrong += 1
-        print(right / right + wrong)
+
+        print(right * 100 / (right + wrong))
 
 
 if __name__ == '__main__':
     classifier = ID3()
-    classifier.test("test.csv")
+    classifier.train()
+    fileName = "test.csv"
+    classifier.test(fileName)
